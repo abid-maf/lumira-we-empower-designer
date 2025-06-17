8ff9dbf00529cfc32acf439718a2d14df0a82cf8
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2025-06-17 14:32:48 +0700

    m

--- src/js/scroll-nav-top.js
+++ src/js/scroll-nav-top.js
@@ -2,12 +2,8 @@ window.addEventListener("scroll", function () {
   const navContainerDark = document.getElementById("NavTopContainerDark");
 
   if (window.scrollY > 814) {
-    navContainerDark.classList.remove("opacity-0");
-    navContainerDark.classList.remove("-z-50");
-    navContainerDark.classList.add("z-50");
+    navContainerDark.classList.add("dark-show");
   } else {
-    navContainerDark.classList.add("opacity-0");
-    navContainerDark.classList.add("-z-50");
-    navContainerDark.classList.remove("z-50");
+    navContainerDark.classList.remove("dark-show");
   }
 });
