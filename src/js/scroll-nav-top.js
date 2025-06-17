99c07c89f954c97c7d148e24a8b1e06d5fc0daec
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2025-06-17 13:07:20 +0700

    continue

--- src/js/scroll-nav-top.js
+++ src/js/scroll-nav-top.js
@@ -1,9 +1,14 @@
 window.addEventListener("scroll", function () {
   const nav = document.getElementById("NavTop");
+  const navContainer = document.getElementById("NavTopContainer");
 
-  if (window.scrollY > 0) {
+  if (window.scrollY > 814) {
     nav.classList.add("scroll");
+    navContainer.classList.remove("top-[60px]");
+    navContainer.classList.add("top-[12px]");
   } else {
     nav.classList.remove("scroll");
+    navContainer.classList.add("top-[60px]");
+    navContainer.classList.remove("top-[12px]");
   }
 });
