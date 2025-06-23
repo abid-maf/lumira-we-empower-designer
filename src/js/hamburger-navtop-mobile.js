a14ac1ba2cd18c002d99f6334e1283ae9bf34958
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2025-06-23 22:21:33 +0700

    m

---
+++ src/js/hamburger-navtop-mobile.js
@@ -0,0 +1,5 @@
+document.getElementById('HamburgerMobileButton').addEventListener('click', function () {
+    const navContainer = document.getElementById('NavTopContainerDarkMobile');
+    navContainer.classList.toggle('hamburger-mobile-toggle');
+  });
+  
