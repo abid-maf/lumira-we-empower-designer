cec90bd8a84c07c51396d488b1fae52d3eb61233
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2025-07-08 22:13:57 +0700

    fix navbar

--- src/js/nav-top-dark.js
+++ src/js/nav-top-dark.js
@@ -1,9 +1,21 @@
 window.addEventListener("scroll", function () {
-  const navContainerDark = document.getElementById("NavTopContainerDark");
+  const navTopContainerDekstop = document.getElementById("NavTopContainerDekstop");
+
+  if (window.scrollY > 414) {
+    navTopContainerDekstop.classList.add("fixed", "top-[-85px]", "pt-0");
+    navTopContainerDekstop.classList.remove("pt-[60px]");
+    if (window.scrollY > 514) {
+      navTopContainerDekstop.classList.add("transition-all", "duration-400");
+    }
+  } else {
+    navTopContainerDekstop.classList.remove("transition-all", "duration-400");
+    navTopContainerDekstop.classList.remove("fixed", "top-[-85px]", "pt-0");
+    navTopContainerDekstop.classList.add("pt-[60px]");
+  }
 
   if (window.scrollY > 814) {
-    navContainerDark.classList.add("dark-show");
+    navTopContainerDekstop.classList.add("scroll");
   } else {
-    navContainerDark.classList.remove("dark-show");
+    navTopContainerDekstop.classList.remove("scroll");
   }
 });
