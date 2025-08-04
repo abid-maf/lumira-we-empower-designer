6c43c4dcbef58f7cd030faf5489620189eaa100d
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2025-08-04 21:18:43 +0700

    progress

---
+++ src/js/modal-input-navbar-top-clear-input-value.js
@@ -0,0 +1,11 @@
+// Ambil elemen input dan tombol "X" menggunakan ID
+const clearModalSearchInput = document.getElementById('modal-search-input');
+const clearInputBtn = document.getElementById('clear-modal-search-input-btn');
+
+// Tambahkan event listener pada tombol "X"
+clearInputBtn.addEventListener('click', () => {
+  // Ketika tombol diklik, kosongkan nilai input
+  clearModalSearchInput.value = '';
+  // Opsional: Fokuskan kembali ke input setelah dikosongkan
+  clearModalSearchInput.focus();
+});
---
+++ src/js/modal-input-navbar-top-toggle.js
@@ -0,0 +1,23 @@
+const openModalBtn = document.getElementById('navbar-top-open-modal-btn');
+const closeModalBtn = document.getElementById('navbar-top-close-modal-btn');
+const navbarTopModal = document.getElementById('navbar-top-modal');
+const modalSearchInput = document.getElementById('modal-search-input');
+
+// Fungsi untuk menampilkan modal
+function openModal() {
+  navbarTopModal.classList.remove('hidden');
+  modalSearchInput.focus();
+}
+
+// Fungsi untuk menyembunyikan modal
+function closeModal() {
+  navbarTopModal.classList.add('hidden');
+  // Tambahkan baris ini untuk mengosongkan nilai input
+  modalSearchInput.value = '';
+}
+
+// Tambahkan event listener pada tombol "Search"
+openModalBtn.addEventListener('click', openModal);
+
+// Tambahkan event listener pada tombol "X"
+closeModalBtn.addEventListener('click', closeModal);
