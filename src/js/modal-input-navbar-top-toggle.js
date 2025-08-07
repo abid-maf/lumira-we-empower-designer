4edb75979d3f67e4e0b566beb5095c270981efbf
tegarfauzan <tegarfauzanfirdaus@gmail.com>
2025-08-07 21:56:25 +0700

    fix

--- src/js/modal-input-navbar-top-toggle.js
+++ src/js/modal-input-navbar-top-toggle.js
@@ -3,10 +3,12 @@ const closeModalBtn = document.getElementById('navbar-top-close-modal-btn');
 const navbarTopModal = document.getElementById('navbar-top-modal');
 const modalSearchInput = document.getElementById('modal-search-input');
 
+const body = document.body;
 // Fungsi untuk menampilkan modal
 function openModal() {
   navbarTopModal.classList.remove('hidden');
   modalSearchInput.focus();
+  body.classList.add('overflow-hidden');
 }
 
 // Fungsi untuk menyembunyikan modal
@@ -14,6 +16,7 @@ function closeModal() {
   navbarTopModal.classList.add('hidden');
   // Tambahkan baris ini untuk mengosongkan nilai input
   modalSearchInput.value = '';
+  body.classList.remove('overflow-hidden');
 }
 
 // Tambahkan event listener pada setiap tombol "Search"
